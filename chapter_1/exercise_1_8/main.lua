1f0d18f13b9f4fb513eff612efede5b4c1210510
Brooks Patton <scgryphon@sbcglobal.net>
2017-04-14 22:13:53 -0600

    set up attraction with an attractor

--- chapter_1/exercise_1_8/main.lua
+++ chapter_1/exercise_1_8/main.lua
@@ -6,32 +6,39 @@ function love.load()
 
     location = vector.new(width / 2, height / 2)
     velocity = vector.new(0, 0)
-
-    topspeed = 5
+    attractor = vector.new(love.math.random(400, 600), love.math.random(200, 400))
 end
 
 function love.draw()
     love.graphics.setColor(255, 255, 255)
     love.graphics.circle('fill', location.x, location.y, 10)
+
+    love.graphics.points(attractor.x, attractor.y)
 end
 
 function love.update(dt)
-    local mousePosition = vector.new(love.mouse.getX(), love.mouse.getY())
+    -- local mousePosition = vector.new(love.mouse.getX(), love.mouse.getY())
 
-    local direction = mousePosition - location
+    local direction = attractor - location
+    local length = direction:len()
+
     direction:normalizeInplace()
 
-    local acceleration = 0.5 * direction
+    local acceleration = direction * calculateMagnitude(length)
 
     velocity = velocity + acceleration * dt
-    limitVelocity()
+
+    velocity = velocity * 0.99
+
     location = location + velocity
 end
 
-function limitVelocity()
-    if velocity.x > topspeed then
-        velocity.x = topspeed
-    elseif velocity.y > topspeed then
-        velocity.y = topst
+function calculateMagnitude(length)
+    local speed = 200 / length
+
+    if speed > 200 then
+        speed = 200
     end
+
+    return speed
 end
