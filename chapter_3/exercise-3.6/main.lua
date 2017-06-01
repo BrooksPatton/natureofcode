6d62bf4f49f25f524ab4a2adb879b50b004976ac
Brooks Patton <scgryphon@sbcglobal.net>
2017-05-31 21:26:51 -0600

    sort of working

--- chapter_3/exercise-3.6/main.lua
+++ chapter_3/exercise-3.6/main.lua
@@ -9,38 +9,27 @@ local twoPi = math.pi * math.pi
 local frameCount
 local location
 local angleIncrement
-local o
+local oscilattors = {}
 
 function love.load()
   width = love.graphics.getWidth()
   height = love.graphics.getHeight()
 
-  location = Vector.new(width/2, height/2)
-  m = Mover.new(Vector.new(0, 0), 10)
-
-  amplitude = 100
-  period = 120
-  frameCount = 0
-  angleIncrement = 5
-  o = Oscillator.new()
+  for i=1, 10 do
+    table.insert(oscilattors, Oscillator.new())
+  end
 end
 
 function love.draw()
-  love.graphics.push()
-  love.graphics.translate(width/2, height/2) 
-  love.graphics.line(0, m.location.y, 0, 0 - height/2)
-  m:draw()
-  love.graphics.setColor({20, 20, 255})
-  love.graphics.pop()
-
-  --love.graphics.line(width/2 + m.location.x, height/2, width/2, 0)
+  for i,o in ipairs(oscilattors) do
+    o:display()
+  end
 end
 
 function love.update(dt)
   if dt > 0.029 then dt = 0.029 end
 
-  --m.location.y = amplitude * math.cos(twoPi * frameCount / period)
-  m.location.y = amplitude * math.cos(m.angle)
-
-  m.angle = m.angle + angleIncrement * dt
+  for i,o in ipairs(oscilattors) do
+    o:update(dt)
+  end
 end
--- chapter_3/exercise-3.6/oscillator.lua
+++ chapter_3/exercise-3.6/oscillator.lua
@@ -8,26 +8,30 @@ function Oscillator.new()
   setmetatable(t, Oscillator)
 
   t.angle = Vector.new(0, 0)
-  t.velocity = Vector.new(love.math.random(-5, 5), love.math.random(-5, 5))
-  t.amplitude = Vector.new(love.math.random(width/2), love.math.random(height/2))
-  t.x
-  t.y
+  t.velocity = Vector.new(love.math.random(-3, 3), love.math.random(-3, 3))
+  t.amplitude = Vector.new(love.math.random(0, width/2), love.math.random(0, height/2))
+  t.x = 0
+  t.y = 0
 
   return t
 end
 
 function Oscillator:update(dt)
-  self.angle = self.angle + self.velocity * dt
+  local v = self.velocity * dt
+  self.angle = self.angle + v
 
-  x = math.sin(self.angle.x) * self.amplitude.x
-  y = math.sin(self.angle.y) * self.amplitude.y
+  self.x = math.sin(self.angle.x) * self.amplitude.x
+  self.y = math.sin(self.angle.y) * self.amplitude.y
 end
 
 function Oscillator:display()
+  love.graphics.push()
+  love.graphics.translate(width/2, height/2)
   love.graphics.setColor({255, 255, 255})
   love.graphics.line(0, 0, self.x, self.y)
   love.graphics.setColor({150, 150, 150})
-  love.graphics.circle('fill', x, y, 16)
+  love.graphics.circle('fill', self.x, self.y, 16)
+  love.graphics.pop()
 end
 
 return Oscillator
