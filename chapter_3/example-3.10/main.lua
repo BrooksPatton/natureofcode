279cd8f2532cb90428ef766ecaae37318a86b72c
Brooks Patton <scgryphon@sbcglobal.net>
2017-06-09 21:30:18 -0600

    added lots of colors

--- chapter_3/example-3.10/main.lua
+++ chapter_3/example-3.10/main.lua
@@ -1,22 +1,38 @@
 local Pendulum = require('./pendulum')
 local Vector = require('./vector')
 
-local pendulum
+local pendulums = {}
 
 function love.load()
   width = love.graphics.getWidth()
   height = love.graphics.getHeight()
+  local noiseOffs = {
+    x = 0,
+    y = 1000,
+    z = 10000,
+    increment = 0.000000001
+  }
 
-  pendulum = Pendulum.new(Vector.new(width/2, 0), height-16)
+  for i=1, 9000 do
+    table.insert(pendulums, Pendulum.new(Vector.new(width/2, 0), love.math.random(20, height-40), noiseOffs))
+
+    noiseOffs.x = noiseOffs.x + 10
+    noiseOffs.y = noiseOffs.y + 10
+    noiseOffs.z = noiseOffs.z + 10
+  end
   gravity = 10
 end
 
 function love.draw()
-  pendulum:draw()
+  for i,pendulum in ipairs(pendulums) do
+    pendulum:draw()
+  end
 end
 
 function love.update(dt)
   if dt > 0.029 then dt = 0.029 end
 
-  pendulum:update(dt)
+  for i,pendulum in ipairs(pendulums) do
+    pendulum:update(dt)
+  end
 end
--- chapter_3/example-3.10/pendulum.lua
+++ chapter_3/example-3.10/pendulum.lua
@@ -3,7 +3,7 @@ local Vector = require('./vector')
 local Pendulum = {}
 Pendulum.__index = Pendulum
 
-function Pendulum.new(origin, armLength)
+function Pendulum.new(origin, armLength, noiseOffs)
   local t = {}
   setmetatable(t, Pendulum)
 
@@ -14,15 +14,21 @@ function Pendulum.new(origin, armLength)
   t.angularVelocity = 0
   t.angularAcceleration = 0
   t.damping = 0.999999
+  t.red = 0
+  t.green = 0
+  t.blue = 0
+  t.armDirection = 'down'
+  t.radius = love.math.random(1, 10)
+  t.noiseOffs = noiseOffs
 
   return t
 end
 
 function Pendulum:draw()
   love.graphics.setColor({255,255,255})
-  love.graphics.line(self.origin.x, self.origin.y, self.location.x, self.location.y)
-  love.graphics.setColor({55,55,55})
-  love.graphics.circle('fill', self.location.x, self.location.y, 16)
+  --love.graphics.line(self.origin.x, self.origin.y, self.location.x, self.location.y)
+  love.graphics.setColor({self.red, self.green, self.blue})
+  love.graphics.circle('fill', self.location.x, self.location.y, self.radius)
 end
 
 function Pendulum:update(dt)
@@ -38,6 +44,14 @@ function Pendulum:update(dt)
   self.location.x = self.armLength * math.sin(self.angle)
   self.location.y = self.armLength * math.cos(self.angle)
   self.location = self.location + self.origin
+
+  self.red = love.math.noise(self.noiseOffs.x) * 255
+  self.green = love.math.noise(self.noiseOffs.x, self.noiseOffs.y) * 255
+  self.blue = love.math.noise(self.noiseOffs.x, self.noiseOffs.y, self.noiseOffs.z) * 255
+
+  self.noiseOffs.x = self.noiseOffs.x + self.noiseOffs.increment
+  self.noiseOffs.y = self.noiseOffs.y + self.noiseOffs.increment
+  self.noiseOffs.z = self.noiseOffs.z + self.noiseOffs.increment
 end
 
 return Pendulum
